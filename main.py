0f632a0d3b60e09497f95c5e7e6df4af268627d1
Kushal Khajuria <kvkhajuria01@gmail.com>
2025-01-13 23:58:14 -0800

    more functionality

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import mediapipe as mp
-import cv2
 from video import Video
-import time
 
 def main():
     vid = Video()
--- video.py
+++ video.py
@@ -1,18 +1,11 @@
 import mediapipe as mp
-from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import cv2
 from functions import *
 
-import json
-import requests
-import random
-
-
 class Video:
     def __init__(self):
         self.base_options = mp.tasks.BaseOptions
-        
         self.face_model_path = 'face_landmarker_v2_with_blendshapes.task'
         self.FaceLandmarker = mp.tasks.vision.FaceLandmarker
         self.FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
@@ -25,19 +18,6 @@ class Video:
         )
 
         self.face_landmarker = self.FaceLandmarker.create_from_options(self.face_options)
-
-        #***********************
-
-        self.pose_model_path = 'pose_landmarker.task'
-        self.PoseLandmarker = mp.tasks.vision.PoseLandmarker
-        self.PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-
-        self.pose_options = self.PoseLandmarkerOptions(
-            base_options=self.base_options(model_asset_path=self.pose_model_path),
-            output_segmentation_masks = True
-        )
-        
-        self.pose_landmarker = vision.PoseLandmarker.create_from_options(self.pose_options)
 
         self.key_landmarks = []
 
@@ -118,95 +98,94 @@ class Video:
 
     def runVideo(self):
         cap = cv2.VideoCapture(0)
-
         num = 1
-        start_detecting = False
-
+        # cv2.waitKey(0)
         while cap.isOpened():
-
-            if cv2.waitKey(1) & 0xFF == ord('s'):
-                start_detecting = True #does nothing for now
-
-            ret, frame = cap.read()
-            image = cv2.flip(frame, 1)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-            
-            face_landmarker_result = self.face_landmarker.detect(image)
-
-            self.l_c_x = int(face_landmarker_result.face_landmarks[0][468].x * 1270) #1440
-            self.l_c_y = int(face_landmarker_result.face_landmarks[0][468].y * 720) #900
-            self.r_c_x = int(face_landmarker_result.face_landmarks[0][473].x * 1270)
-            self.r_c_y = int(face_landmarker_result.face_landmarks[0][473].y * 720)
-
-            self.l_l_x = int(face_landmarker_result.face_landmarks[0][469].x * 1270)
-            self.l_l_y = int(face_landmarker_result.face_landmarks[0][469].y * 720)
-            self.r_l_x = int(face_landmarker_result.face_landmarks[0][474].x * 1270)
-            self.r_l_y = int(face_landmarker_result.face_landmarks[0][474].y * 720)
-
-            self.l_t_x = int(face_landmarker_result.face_landmarks[0][470].x * 1270)
-            self.l_t_y = int(face_landmarker_result.face_landmarks[0][470].y * 720)
-            self.r_t_x = int(face_landmarker_result.face_landmarks[0][475].x * 1270)
-            self.r_t_y = int(face_landmarker_result.face_landmarks[0][475].y * 720)
-
-            self.l_r_x = int(face_landmarker_result.face_landmarks[0][471].x * 1270)
-            self.l_r_y = int(face_landmarker_result.face_landmarks[0][471].y * 720)
-            self.r_r_x = int(face_landmarker_result.face_landmarks[0][476].x * 1270)
-            self.r_r_y = int(face_landmarker_result.face_landmarks[0][476].y * 720)
-
-            self.l_b_x = int(face_landmarker_result.face_landmarks[0][472].x * 1270)
-            self.l_b_y = int(face_landmarker_result.face_landmarks[0][472].y * 720)
-            self.r_b_x = int(face_landmarker_result.face_landmarks[0][477].x * 1270)
-            self.r_b_y = int(face_landmarker_result.face_landmarks[0][477].y * 720)
-
-            self.l_ll_x = int(face_landmarker_result.face_landmarks[0][33].x * 1270)
-            self.l_ll_y = int(face_landmarker_result.face_landmarks[0][33].y * 720)
-            self.l_rr_x = int(face_landmarker_result.face_landmarks[0][133].x * 1270)
-            self.l_rr_y = int(face_landmarker_result.face_landmarks[0][133].y * 720)
-
-            self.r_ll_x = int(face_landmarker_result.face_landmarks[0][362].x * 1270)
-            self.r_ll_y = int(face_landmarker_result.face_landmarks[0][362].y * 720)
-            self.r_rr_x = int(face_landmarker_result.face_landmarks[0][263].x * 1270)
-            self.r_rr_y = int(face_landmarker_result.face_landmarks[0][263].y * 720)
-
-            self.updateVariables()
-
-            #**************************************************************************
-
-            image = cv2.cvtColor(image.numpy_view(), cv2.COLOR_RGB2BGR)
-
-            self.putEyeDots(image, self.FONT, self.COLOR)
-
-            cv2.namedWindow('image', cv2.WINDOW_NORMAL) #cv2.WINDOW_AUTOSIZE or cv2.WINDOW_NORMAL if you want to resize
-            cv2.resizeWindow('image', 1440, 810)
-
-            start_point = (640, 0)
-            end_point = (640, 720)
-            color = (0, 255, 0)
-            thickness = 9
-
-            image = cv2.line(image, start_point, end_point, color, thickness)
-
-            self.testAccuracy(image)
-
-            cv2.imshow('image', image)
-
+            self.regularFrame(cap, True)
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-                cv2.imwrite('image.jpg', image)
+        self.infiniteBlackFramesDiscrete(cap, num)
 
-                frame = np.zeros(shape=[900, 1440, 3], dtype=np.uint8)
+        # self.infiniteBlackFramesContinuous(cap, num)
 
-                self.putEyeDots(frame, self.FONT, self.COLOR)
+        # self.oneBlackFrame(cap, num)
 
-                cv2.imshow(f'eye frame {num}', frame)
+        # cap.release()
+        # cv2.destroyAllWindows()
 
-                num+=1
+    def infiniteBlackFramesDiscrete(self, cap, num):
+        while cap.isOpened():
+            self.blackFrame(cap, num, True)
+            while True:
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    self.blackFrame(cap, num, True)
+                    break
+            num+=1
 
-                # break
+    def infiniteBlackFramesContinuous(self, cap, num):
+        while cap.isOpened():
+            while True:
+                self.blackFrame(cap, num, True)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            num+=1
+            cv2.waitKey(0)
 
-        cap.release()
-        cv2.destroyAllWindows()        
+    def oneBlackFrame(self, cap, num):
+        while cap.isOpened():
+            self.blackFrame(cap, num, True)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cv2.waitKey(0)
+
+    def readFrame(self, cap):
+        ret, frame = cap.read()
+        image = cv2.flip(frame, 1)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+
+    def regularFrame(self, cap, display):
+        image = self.readFrame(cap)
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+        face_landmarker_result = self.face_landmarker.detect(image)
+        image = cv2.cvtColor(image.numpy_view(), cv2.COLOR_RGB2BGR)
+        self.UI(face_landmarker_result, image, display)
+
+    def UI(self, face_landmarker_result, image, display):
+        self.defineAndScale(face_landmarker_result, 1280, 720)
+        self.putEyeDots(image, self.FONT, self.COLOR)
+        self.lineThroughMiddle(image)
+        self.testAccuracy(image)
+
+        if display:
+            self.UiDisplay(image)
+
+    def UiDisplay(self, image):
+        cv2.namedWindow('image', cv2.WINDOW_NORMAL) #cv2.WINDOW_AUTOSIZE, or cv2.WINDOW_NORMAL if you want to resize
+        cv2.resizeWindow('image', 1440, 810)
+        cv2.imshow('image', image)
+
+    def blackFrame(self, cap, num, display):
+        print("yay!!!")
+        image = self.readFrame(cap)
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+        face_landmarker_result = self.face_landmarker.detect(image)
+        frame = np.zeros(shape=[720, 1280, 3], dtype=np.uint8) #900, 1440; 1693, 2880
+        self.blackAndWhite(face_landmarker_result, frame, num, display)
+
+    def blackAndWhite(self, face_landmarker_result, image, num, display):
+        self.defineAndScale(face_landmarker_result, 1280, 720) #1493, 2560
+        self.putEyeDots(image, self.FONT, self.COLOR)
+
+        if display:
+            self.blackAndWhiteDisplay(num, image)
+
+    def blackAndWhiteDisplay(self, num, image):
+        cv2.namedWindow(f'eye frame {num}', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(f'eye frame {num}', 1440, 810)
+        cv2.imshow(f'eye frame {num}', image)
 
     def putEyeDots(self, frame, font, color):
         for i in range(len(self.key_landmarks)):
@@ -265,3 +244,49 @@ class Video:
             cv2.putText(image, 'YES', yes_point, self.FONT, 3, color, 3, cv2.LINE_AA)
         else:
             cv2.putText(image, 'NO', no_point, self.FONT, 3, color, 3, cv2.LINE_AA)
+
+    def defineAndScale(self, face_landmarker_result, scale_x, scale_y):
+        self.l_c_x = int(face_landmarker_result.face_landmarks[0][468].x * scale_x) #1440
+        self.l_c_y = int(face_landmarker_result.face_landmarks[0][468].y * scale_y) #900
+        self.r_c_x = int(face_landmarker_result.face_landmarks[0][473].x * scale_x)
+        self.r_c_y = int(face_landmarker_result.face_landmarks[0][473].y * scale_y)
+
+        self.l_l_x = int(face_landmarker_result.face_landmarks[0][469].x * scale_x)
+        self.l_l_y = int(face_landmarker_result.face_landmarks[0][469].y * scale_y)
+        self.r_l_x = int(face_landmarker_result.face_landmarks[0][474].x * scale_x)
+        self.r_l_y = int(face_landmarker_result.face_landmarks[0][474].y * scale_y)
+
+        self.l_t_x = int(face_landmarker_result.face_landmarks[0][470].x * scale_x)
+        self.l_t_y = int(face_landmarker_result.face_landmarks[0][470].y * scale_y)
+        self.r_t_x = int(face_landmarker_result.face_landmarks[0][475].x * scale_x)
+        self.r_t_y = int(face_landmarker_result.face_landmarks[0][475].y * scale_y)
+
+        self.l_r_x = int(face_landmarker_result.face_landmarks[0][471].x * scale_x)
+        self.l_r_y = int(face_landmarker_result.face_landmarks[0][471].y * scale_y)
+        self.r_r_x = int(face_landmarker_result.face_landmarks[0][476].x * scale_x)
+        self.r_r_y = int(face_landmarker_result.face_landmarks[0][476].y * scale_y)
+
+        self.l_b_x = int(face_landmarker_result.face_landmarks[0][472].x * scale_x)
+        self.l_b_y = int(face_landmarker_result.face_landmarks[0][472].y * scale_y)
+        self.r_b_x = int(face_landmarker_result.face_landmarks[0][477].x * scale_x)
+        self.r_b_y = int(face_landmarker_result.face_landmarks[0][477].y * scale_y)
+
+        self.l_ll_x = int(face_landmarker_result.face_landmarks[0][33].x * scale_x)
+        self.l_ll_y = int(face_landmarker_result.face_landmarks[0][33].y * scale_y)
+        self.l_rr_x = int(face_landmarker_result.face_landmarks[0][133].x * scale_x)
+        self.l_rr_y = int(face_landmarker_result.face_landmarks[0][133].y * scale_y)
+
+        self.r_ll_x = int(face_landmarker_result.face_landmarks[0][362].x * scale_x)
+        self.r_ll_y = int(face_landmarker_result.face_landmarks[0][362].y * scale_y)
+        self.r_rr_x = int(face_landmarker_result.face_landmarks[0][263].x * scale_x)
+        self.r_rr_y = int(face_landmarker_result.face_landmarks[0][263].y * scale_y)
+
+        self.updateVariables()
+
+    def lineThroughMiddle(self, image):
+        start_point = (640, 0)
+        end_point = (640, 720)
+        color = (0, 255, 0)
+        thickness = 9
+
+        image = cv2.line(image, start_point, end_point, color, thickness)
